7d26d66de58f6585fcd9ca822e816d4d1ad189f9
Yeetle <jonas.tolkemitt@gmail.com>
2022-12-18 11:33:04 +0100

    Add Delays for Picnic.

--- Config/Delays.h
+++ Config/Delays.h
@@ -15,21 +15,47 @@
 //PokemonSV delays ---------------------------
 #define PokemonSV_STARTUP_DELAY 244
 
+    //general delays
+#define PokemonSV_OPEN_MENU_DELAY 122
 #define PokemonSV_NAVIGATION_DELAY 20
-
 #define PokemonSV_SPAWN_POPUP_DELAY 61
-#define PokemonSV_SELECT_POKEMON_DELAY 122
 
+    // pokemon select delays
+#define PokemonSV_SELECT_POKEMON_DELAY 122
+#define PokemonSV_PUT_AWAY_HELD_ITEM_DELAY 61
 #define PokemonSV_RETURN_TO_RIDE_DELAY 122
 #define PokemonSV_READY_TO_TRAVEL_DELAY 300
 
+    // box delays
 #define PokemonSV_OPEN_BOX_DELAY 244
 #define PokemonSV_CLOSE_BOX_DELAY 244
 #define PokemonSV_CHANGE_SELECTION_MODE_DELAY 30
 #define PokemonSV_CHANGE_BOX_DELAY 61
-#define PokemonSV_PUT_AWAY_HELD_ITEM_DELAY 60
 
-#define PokemonSV_OPEN_MENU_DELAY 122
+    // picnic delays
+#define PokemonSV_PICNIC_TABLE_INTERACTION_DELAY 122
+#define PokemonSV_OPEN_SANDWICH_MENU_DELAY 732
+#define PokemonSV_SELECT_SANDWICH_DELAY 61
+#define PokemonSV_START_SANDWICH_MINIGAME_DELAY 900
+#define PokemonSV_READY_TO_PLACE_BUN_DELAY 244
+#define PokemonSV_PLACE_BUN_DELAY 488
+#define PokemonSV_SANDWICH_DISPLAY_ANIMATION_DELAY 2000
+#define PokemonSV_SANDWICH_EAT_ANIMATION_DELAY 3600
+#define PokemonSV_CLOSE_SANDWICH_MINIGAME_DELAY 244
+
+    // picnic controls
+#define PokemonSV_PICNIC_TURN_POSITION_UP 108
+#define PokemonSV_PICNIC_TURN_POSITION_DOWN 148
+#define PokemonSV_PICNIC_TURN_HOLD_DURATION 10
+#define PokemonSV_TURN_AROUND_DELAY 61
+
+
+    // sandwich minigame controls
+#define PokemonSV_SANDWICH_INITIAL_STICK_HOLD_DURATION 20
+#define PokemonSV_SANDWICH_STICK_HOLD_DURATION 135
+#define PokemonSV_SANDWICH_STICK_POSITION_UP 64
+#define PokemonSV_SANDWICH_STICK_POSITION_DOWN 191
+#define PokemonSV_SANDWICH_MOVE_HAND_DELAY 122
 
 
 
